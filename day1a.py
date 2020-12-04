f6bb23e48db0e7a091719a885c63b48713734709
Antoine Gruzelle <antogruz@gmail.com>
2020-12-04 17:53:05 +0100

    Day1

---
+++ day1a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def main():
+    numbers = readFile("inputs/day1a.txt")
+    print(numbers)
+    a, b = findPair(numbers, 2020)
+    print(a * b)
+
+    a, b, c = findTriplet(numbers, 2020)
+    print(a * b * c)
+
+def readFile(f):
+    with open(f, 'r') as fh:
+        return [int(l) for l in fh.readlines()]
+
+
+def findPair(numbers, sum):
+    for a in numbers:
+        for b in numbers:
+            if a + b == sum:
+                return a, b
+
+def findTriplet(numbers, sum):
+    for a in numbers:
+        for b in numbers:
+            for c in numbers:
+                if a + b + c == sum:
+                    return a, b, c
+
+main()
