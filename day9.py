c5e5e616e3de9f892fd73190bb8ceb124b00d235
Antoine Gruzelle <antogruz@gmail.com>
2024-12-11 18:20:54 +0100

    day9 part2

--- day9.py
+++ day9.py
@@ -6,6 +6,8 @@ from input import *
 def main():
     text = readFile("inputs/day9.txt").strip()
     print(checksum(rebalance(expand(text))))
+    print(checksum(newRebalance(expand(text))))
+
 
 def checksum(l):
     result = 0
@@ -14,12 +16,50 @@ def checksum(l):
             result += i * int(element)
     return result
 
-
-
 def rebalance(ram):
     while not balanced(ram):
         balanceStep(ram)
     return ram
+
+def newRebalance(ram):
+    start = findHighestNumber(ram)
+    for i in range(int(start), 0, -1):
+        ram = shiftValue(ram, str(i))
+    return ram
+
+def findHighestNumber(l):
+    n = 0
+    for e in l:
+        if e == ".":
+            continue
+        if int(e) > int(n):
+            n = e
+    return n
+
+def shiftValue(l, value):
+    indices = findIndices(l, value)
+    newSpot = findEmptySpot(l, len(indices))
+    if newSpot == -1 or newSpot > indices[0]:
+        return l
+    for increment, index in enumerate(indices):
+        l = permute(l, newSpot + increment, index)
+    return l
+
+def findIndices(l, value):
+    return [ i for i, x in enumerate(l) if x == value ]
+
+def findEmptySpot(l, size):
+    for i, element in enumerate(l):
+        if nextElementsAreEmpty(l, i, size):
+            return i
+    return -1
+
+def nextElementsAreEmpty(l, index, size):
+    for i in range(size):
+        if index + i >= len(l) or l[index + i] != ".":
+            return False
+    return True
+
 
 def balanced(ram):
     firstPointIndex = ram.index(".")
@@ -38,13 +78,11 @@ def findLastId(l):
         if l[i] != ".":
             return i
 
-
 def permute(l, i, j):
     buffer = l[i]
     l[i] = l[j]
     l[j] = buffer
     return l
-
 
 def expand(s):
     disk = True
@@ -63,14 +101,16 @@ def appendMultipleTimes(l, element, count):
     for i in range(count):
         l.append(element)
 
-
-
 class Tester:
     def testExpander(self):
         assertEquals(toList("0..111....22222"), expand("12345"))
 
     def testRebalancer(self):
         assertEquals(toList("022111222......"), toList(rebalance(expand("12345"))))
+
+    def testNewRebalancer(self):
+        assertEquals(toList("00992111777.44.333....5555.6666.....8888.."), toList(newRebalance(expand("2333133121414131402"))))
+
 
 
 def toList(s):
